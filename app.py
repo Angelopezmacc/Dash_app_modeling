0a4ac0334a9c0358ae3fcedc42dde44173ffd8cd
angelopez_macc <angel.lopez@urosario.edu.co>
2023-09-18 22:43:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import numpy as np
-from sympy import symbols, Eq, solve
+from sympy import symbols, Eq, solve, Matrix
 
 app = dash.Dash(__name__)
 
@@ -20,12 +20,13 @@ app.layout = html.Div([
                      style={'width': '100%', 'height': 100}),
         html.Button('Calcular Solución', id='calculate-button', n_clicks=0),
     ]),
+    html.Div(id='system-equations', className='small-note'),
     html.Div(id='solution-output', className='small-note'),
 ])
 
 # Lógica de la aplicación
 @app.callback(
-    Output('solution-output', 'children'),
+    [Output('system-equations', 'children'), Output('solution-output', 'children')],
     [Input('calculate-button', 'n_clicks')],
     [dash.dependencies.State('input-matrix', 'value')]
 )
@@ -35,24 +36,32 @@ def calcular_solucion(n_clicks, matrix_text):
             # Parsea la matriz ingresada como una matriz numpy
             matrix = np.array(eval(matrix_text))
             
+            # Convierte la matriz a una matriz sympy para representación matemática
+            sympy_matrix = Matrix(matrix)
+            
             # Convierte la matriz a símbolos
-            symbols_list = [symbols(f'x{i}') for i in range(len(matrix))]
+            symbols_list = symbols('x0:%d' % sympy_matrix.shape[1])
             
             # Crea un sistema de ecuaciones
-            equations = [Eq(np.dot(matrix[i, :], symbols_list), 0) for i in range(len(matrix))]
-            
+            equations = [Eq(sympy_matrix * Matrix(symbols_list), Matrix(np.zeros(sympy_matrix.shape[0])))]
+
             # Resuelve el sistema de ecuaciones
             solution = solve(equations, symbols_list)
+            
+            # Formatea la representación matemática del sistema de ecuaciones
+            system_eq_str = [str(eq) for eq in equations]
             
             # Formatea la solución como una cadena
             solution_str = ', '.join([f'{symbol} = {value}' for symbol, value in solution.items()])
             
-            return f'Solución general: {solution_str}'
+            return system_eq_str, f'Solución general: {solution_str}'
         except Exception as e:
-            return f'Error al calcular la solución: {str(e)}'
+            return f'Error al calcular la solución: {str(e)}', ''
+    return '', ''
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 
 
 
